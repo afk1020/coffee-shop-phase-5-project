fb8966def2679f602e711df72be382a4a09ca60b
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-07-09 10:00:54 -0500

    navbar updated

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -1,65 +1,65 @@
-// import React, { useState } from "react";
-// import Form from "react-bootstrap/Form";
-// import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-// function LoginForm({ onLogin }) {
-//   let [name, setName] = useState("");
-//   let [password, setPassword] = useState("");
-//   let [errors, setErrors] = useState([]);
-//   let [isLoading, setIsLoading] = useState(false);
+function LoginForm({ onLogin }) {
+  let [name, setName] = useState("");
+  let [password, setPassword] = useState("");
+  let [errors, setErrors] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
 
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     setIsLoading(true);
-//     fetch("/login", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ name, password }),
-//     }).then((r) => {
-//       setIsLoading(false);
-//       if (r.ok) {
-//         r.json().then((user) => onLogin(user));
-//       } else {
-//         r.json().then((err) => setErrors(err.errors));
-//       }
-//     });
-//   }
-//   // handleLogoutClick(() => {
-//   //   fetch("/logout", { method: "DELETE" }).then((r) => {
-//   //     if (r.ok) {
-//   //       setUser(null);
-//   //     }
-//   //   });
-//   // }
-//   return (
-//     <div className="Login">
-//     <Form onSubmit={handleSubmit}>
-//       <Form.Group size="lg" controlId="name">
-//         <Form.Label>Name</Form.Label>
-//         <Form.Control
-//           autoFocus
-//           type="name"
-//           value={name}
-//           onChange={(e) => setName(e.target.value)}
-//         />
-//       </Form.Group>
-//       <Form.Group size="lg" controlId="password">
-//         <Form.Label>Password</Form.Label>
-//         <Form.Control
-//           type="password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//       </Form.Group>
-//       <Button block size="lg" type="submit" >
-//       {isLoading ? "Loading..." : "Login"}
-//      </Button>
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, password }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+  // handleLogoutClick(() => {
+  //   fetch("/logout", { method: "DELETE" }).then((r) => {
+  //     if (r.ok) {
+  //       setUser(null);
+  //     }
+  //   });
+  // }
+  return (
+    <div className="Login">
+    <Form onSubmit={handleSubmit}>
+      <Form.Group size="lg" controlId="name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          autoFocus
+          type="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group size="lg" controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button block size="lg" type="submit" >
+      {isLoading ? "Loading..." : "Login"}
+     </Button>
    
-//     </Form>
-//   </div>    
-//   );
-// }
+    </Form>
+  </div>    
+  );
+}
 
-// export default LoginForm;
+export default LoginForm;
