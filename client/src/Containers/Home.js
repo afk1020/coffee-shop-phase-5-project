36e549af4b024cdae892919898bd43c90f930655
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-07-13 16:35:28 -0500

    final

--- client/src/Containers/Home.js
+++ client/src/Containers/Home.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div className="Home">
       <div className="lander">
         <h1>OneStopCoffee</h1>
+        <img src="https://cdn.luxe.digital/media/2020/05/07203610/best-coffee-beans-luxe-digital.jpg"></img>
         <p className="text-muted">All your coffee needs</p>
       </div>
     </div>
